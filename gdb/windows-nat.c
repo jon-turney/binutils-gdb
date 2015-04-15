dd226c8f2634c2b44ead586984710be068da4bfe
Jon Turney <jon.turney@dronecode.org.uk>
2015-04-15 21:41:25 +0100

    Don't change current_event.dwThreadId in handle_output_debug_string()
    Since a Cygwin signal may be reported by a different thread to the thread the
    signal is to be delivered to, use the signal target thread id by returning it,
    rather than re-writing the thread id in current_event.
    
    Altering current_event.dwThreadId() will cause ContinueDebugEvent() to be
    applied to the wrong thread and fail, leaving the actual thread which reported
    the debug event stuck in the suspended state.

--- gdb/windows-nat.c
+++ gdb/windows-nat.c
@@ -859,7 +859,6 @@ handle_output_debug_string (struct target_waitstatus *ourstatus)
 					 __COPY_CONTEXT_SIZE, &n)
 		   && n == __COPY_CONTEXT_SIZE)
 	    signal_thread_id = retval;
-	  current_event.dwThreadId = retval;
 	}
     }
 #endif
@@ -1521,7 +1520,7 @@ get_windows_debug_event (struct target_ops *ops,
 				  thread_id);
       current_thread = th;
       if (!current_thread)
-	current_thread = thread_rec (current_event.dwThreadId, TRUE);
+	current_thread = thread_rec (thread_id, TRUE);
     }
 
 out:
